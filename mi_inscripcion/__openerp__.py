4800785f30a4e1ffb9c4d5355bcffb5785176f8a
Juan Perea <jpereac@gmail.com>
2018-05-21 13:44:44 +0200

    website change

--- mi_inscripcion/__openerp__.py
+++ mi_inscripcion/__openerp__.py
@@ -4,7 +4,7 @@
     "version" : "1.0",
     "author" : "Juan Perea Campos & Felipe Lozano Romero",
     "category" : "Generic Modules/Others",
-    "website" : "https://github.com/jupcan",
+    "website" : "https://github.com/jupcan/odoo_modules",
     "description": "módulo para la gestión del estado de una iscripción",
     "depends" : ["base"],
     "update_xml" : ["inscripcion_view.xml"],
